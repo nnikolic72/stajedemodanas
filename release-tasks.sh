423652f639549de50752da63289d924d8507dd57
nenad <nnenad@gmail.com>
2018-11-17 13:19:49 +0100

    trying out heroku release

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,2 +1,9 @@
-python manage.py collectstatic --no-input
-python manage.py migrate
+#!/bin/bash
+
+echo "Running Release Tasks"
+
+echo "Running Migrations"
+
+./manage.py collectstatic --no-input
+./python manage.py migrate
+echo "Done"
